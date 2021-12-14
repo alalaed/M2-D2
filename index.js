aa7edc0c258a7f9a31c8904cdbfa591d249344fa
Ali Alaeddine <a.alaeddine@live.com>
2021-12-14 15:01:36 +0100

    Css

--- index.js
+++ index.js
@@ -3,21 +3,21 @@
 
 
 
-const genres = ["Comedy", "Drama", "Romance", "Horror", "Sci-Fi", "Documentary"]
+const genres = ["HipHop", "Rap", "Jazz", "Alt.Rock", "RnB", "Pop"]
 
 
 
-const listing = function(){
-    let list = document.createElement("ul")
-    let bodyContainer= document.querySelector("body")
-    bodyContainer.appendChild(list)
 
-    for( i=0; i<genres.length; i++){
-        let item = document.createElement("li")
-        item.innerText=genres[i]
-        list.appendChild(item)
-    }
+let list = document.createElement("ul")
+let bodyContainer= document.querySelector("body")
+bodyContainer.appendChild(list)
+
+for( i=0; i<genres.length; i++){
+    let item = document.createElement("li")
+    item.innerText=genres[i]
+    list.appendChild(item)
 }
+
 
 
 
